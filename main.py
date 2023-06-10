05c59b1a4690fe2e1f6ff68c3b8cefd1dc65607d
ProgrammerShahJalal <you@example.com>
2023-06-10 10:08:29 +0600

    loan and calculation

--- main.py
+++ main.py
@@ -36,17 +36,17 @@ class User:
 class Admin:
     def __init__(self):
         self.users = []
-        self.total_balance = 0
+        self.total_bank_balance = 0
         self.total_loan_amount = 0
         self.loan_feature_enabled = True
 
     def create_account(self, name, account_number, balance=0):
         new_user = User(name, account_number, balance)
         self.users.append(new_user)
-        self.total_balance += balance
+        self.total_bank_balance += balance
 
-    def check_total_balance(self):
-        return self.total_balance
+    def check_total_bank_balance(self):
+        return self.total_bank_balance
 
     def check_total_loan_amount(self):
         return self.total_loan_amount
@@ -59,13 +59,58 @@ class Admin:
 
     def perform_loan(self, user):
         if self.loan_feature_enabled:
-            loan_amount = user.balance * 2
+            loan_amount = user.balance*2
             user.balance += loan_amount
             user.loan_amount += loan_amount
-            self.total_balance += loan_amount
+            self.total_bank_balance += loan_amount
             self.total_loan_amount += loan_amount
             user.transactions.append(f"Loan: +{loan_amount}")
             return loan_amount
         else:
             return 0
 
+
+admin = Admin()
+admin.create_account("Shah Jalal", "IIBL-35346", 1000)
+admin.create_account("Farabi", "IIBL-67890", 500)
+admin.create_account("Shaimum", "IIBL-23469", 1500)
+admin.create_account("Mamun", "IIBL-64381", 24000)
+
+user1, user2, user3, user4 = admin.users
+
+print(f"The balance of {user1.name}: {user1.check_balance()}") 
+print(f"The balance of {user2.name}: {user2.check_balance()}")
+print(f"The balance of {user3.name}: {user3.check_balance()}")
+print(f"The balance of {user4.name}: {user4.check_balance()}")
+
+print()
+user1.deposit(900)
+user1.withdraw(100)
+user1.transfer(200, user2)
+user2.deposit(400)
+user3.withdraw(500)
+user4.transfer(18000, user1)
+
+print(f"The balance of {user1.name}: {user1.check_balance()}") 
+print(f"The balance of {user2.name}: {user2.check_balance()}")
+print(f"The balance of {user3.name}: {user3.check_balance()}")
+print(f"The balance of {user4.name}: {user4.check_balance()}")
+
+print()
+
+print(f"The transaction history of {user1.name}: {user1.check_transaction_history()}")
+print(f"The transaction history of {user2.name}: {user2.check_transaction_history()}")
+print(f"The transaction history of {user3.name}: {user3.check_transaction_history()}")
+print(f"The transaction history of {user4.name}: {user4.check_transaction_history()}")
+
+print()
+
+admin.enable_loan_feature()
+loan_amount = admin.perform_loan(user4)
+print(f"The loan amount of {user4.name}: {loan_amount}")
+print(f"{user1.name} balance: {user1.check_balance()}")
+print(f"{user2.name} balance: {user2.check_balance()}")
+print(f"{user3.name} balance: {user3.check_balance()}")
+print(f"{user4.name} balance: {user4.check_balance()}")
+print("The Total loan amount:", admin.check_total_loan_amount())
+print("The total available balance of the bank:", admin.check_total_bank_balance())
